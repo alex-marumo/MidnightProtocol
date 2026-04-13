83f8b5662233a4daa5bb1c20cad6aa1b2dabaf11
alex-marumo <alexmarumo16@gmail.com>
2026-04-13 20:35:08 +0200

    few color tweaks and xray on ags widgets

--- .config/waypaper/scripts/refresh_shell.sh
+++ .config/waypaper/scripts/refresh_shell.sh
@@ -8,6 +8,6 @@ export XDG_RUNTIME_DIR="/run/user/$(id -u)"
 WALLPAPER=$(grep '^wallpaper' ~/.config/waypaper/config.ini | head -1 |
   sed 's/wallpaper = //' | sed "s|~|$HOME|" | xargs)
 [[ -z "$WALLPAPER" || ! -f "$WALLPAPER" ]] && exit 1
-
+ln -sf "$(strings ~/.cache/awww/0.12.0/eDP-1 | grep -oP '/.*')" "$HOME/Pictures/wallpapers/.current_wallpaper"
 matugen image "$WALLPAPER" --mode dark -t scheme-expressive --source-color-index 0
 true
